757404688f1987a582537adcce42f6f6a2ea3bd8
Androw_Ebrahim <123977519+androw100@users.noreply.github.com>
2024-07-17 16:10:18 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+AOS.init();
